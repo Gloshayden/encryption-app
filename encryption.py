8b6107ac5ed14ab572c0a2741d9b157c2bd32887
Gloshayden <Gloshayden@gmail.com>
2025-09-04 18:06:17 +0100

    finished the core app with verification for regenerating keys and decrypting files

--- encryption.py
+++ encryption.py
@@ -1,4 +1,4 @@
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet, InvalidToken
 
 if __name__ == "__main__":
     print("This is a library \nPlease import it and not run it")
@@ -12,14 +12,22 @@ def encryptFile(FileName):
     with open(FileName,"rb") as f:
         Original = f.read()
     encoded_file = cipher_suite.encrypt(Original)
+    try:
+        cipher_suite.decrypt(Original)
+        return "not encrypted"
+    except InvalidToken:
+        pass
     with open(FileName,"wb") as f:
         f.write(encoded_file)
-    return encoded_file
+    return "encrypted"
 
 def decryptFile(FileName):
     with open(FileName,"rb") as f:
         Encrypted = f.read()
-    decoded_file = cipher_suite.decrypt(Encrypted)
+    try:
+        decoded_file = cipher_suite.decrypt(Encrypted)
+    except InvalidToken:
+        return "not decrypted"
     with open(FileName,"wb") as f:
         f.write(decoded_file)
-    return decoded_file
+    return "decrypted"
--- main.py
+++ main.py
@@ -2,6 +2,20 @@ import os, encryption, json, hashlib
 import FreeSimpleGUI as sg
 from cryptography.fernet import Fernet
 
+def askForPassword(password) -> bool:
+    layout = [  [sg.Text("Please enter your password")],
+                [sg.InputText()],
+                [sg.Button('Confirm')] ]
+    window = sg.Window('conform password', layout)
+    event, values = window.read()
+    prehash = values[0]
+    window.close()
+    hashedpass = hashlib.sha256(prehash.encode()).hexdigest()
+    if hashedpass == password:
+        return True
+    else:
+        return False
+
 def main():
     if not os.path.exists("files"): os.mkdir("files")
     if not os.path.exists("settings.json"):
@@ -12,6 +26,7 @@ def main():
         window = sg.Window('Make Password', layout)
         event, values = window.read()
         password = values[0]
+        window.close()
         settings = {"password":hashlib.sha256(password.encode()).hexdigest()}
         with open("settings.json","w") as file:
             json.dump(settings, file)
@@ -19,19 +34,85 @@ def main():
         key = Fernet.generate_key()
         with open("key.key","wb") as f:
             f.write(key)
+
     with open("key.key","rb") as f:
         keyinfo = f.read()
     encryption.initdecryption(keyinfo)
-    while app := True:
+
+    with open("settings.json","r") as f:
+        settings = json.load(f)
+    password = settings["password"]
+
+    while True:
         files = os.listdir("files")
         layout = [  [sg.Text("Please select the file you want to encrypt or decrypt")],
                     [sg.Listbox(values=files, size=(50,20), select_mode="LISTBOX_SELECT_MODE_SINGLE", bind_return_key=True)],
-                    [sg.Button("Regenerate Key", key="key"), sg.Button("Exit")]]
+                    [sg.Button("Regenerate Key", key="key"),sg.Button("Refresh") ,sg.Button("Exit")]]
         window = sg.Window('Please pick a file', layout)
         event, values = window.read()
         if event == sg.WIN_CLOSED or event == "Exit":
             window.close()
             break
+
+        elif event == "Refresh":
+            window.close()
+            continue
+
+        elif event == "key":
+            window.close()
+            layout = [  [sg.Text("Are you sure you want to regenerate the key?")],
+                        [sg.Button("Yes"),sg.Button("No")]]
+            window = sg.Window('regenerate key?', layout)
+            event, values = window.read()
+            if event == "Yes":
+                window.close()
+                PassCheck = askForPassword(password)
+                if PassCheck == True:
+                    os.remove("key.key")
+                    key = Fernet.generate_key()
+                    with open("key.key","wb") as f:
+                        f.write(key)
+                    with open("key.key","rb") as f:
+                        keyinfo = f.read()
+                        encryption.initdecryption(keyinfo)
+                else:
+                    sg.popup("Incorrect password")
+            else:
+                window.close()
+                continue
+            key = Fernet.generate_key()
+            with open("key.key","wb") as f:
+                f.write(key)
+            with open("key.key","rb") as f:
+                keyinfo = f.read()
+                encryption.initdecryption(keyinfo)
+
+        elif values != {0: []}:
+            window.close()
+            file = values[0][0]
+            layout = [  [sg.Text("What do you want to do with this file?")],
+                        [sg.Text("File: " + file)],
+                        [sg.Button("Encrypt"),sg.Button("Decrypt"), sg.Button("Back")]]
+            window = sg.Window('Please pick a file', layout)
+            event, values = window.read()
+            window.close()
+            if event == "Encrypt":
+                result = encryption.encryptFile("files/"+file)
+                if result == "encrypted":
+                    sg.popup("File encrypted")
+                else:
+                    sg.popup("File already encrypted")
+            elif event == "Decrypt":
+                PassCheck = askForPassword(password)
+                if PassCheck == True:
+                    result = encryption.decryptFile("files/"+file)
+                    if result == "decrypted":
+                        sg.popup("File decrypted")
+                    else:
+                        sg.popup("File already decrypted")
+                else:
+                    sg.popup("Incorrect password")
+                continue
 
 
 if __name__ == "__main__":
